a72009f0121dea0fa800c2e7fa156fcf504d332b
ThunderisticRyan <83451100+ThunderisticRyan@users.noreply.github.com>
2022-01-10 18:35:16 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2G--CECvARUOvChLKgL5XajC0GipUfeA",
+    authDomain: "mycity-wkmk.firebaseapp.com",
+    projectId: "mycity-wkmk",
+    storageBucket: "mycity-wkmk.appspot.com",
+    messagingSenderId: "340421583562",
+    appId: "1:340421583562:web:849ad223e75dae5487ca33",
+    measurementId: "G-5PC31XL1VP"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
